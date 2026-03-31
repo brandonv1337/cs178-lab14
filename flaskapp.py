09559a65dc242db06e368e271d1343331ad37c36
brandonv1337 <158694624+brandonv1337@users.noreply.github.com>
2026-03-31 11:32:31 -0500

    Exercise 2

--- flaskapp.py
+++ flaskapp.py
@@ -76,6 +76,23 @@ def viewdb():
 
 # TODO: Section 2 — add your /artistquery/<artist_id> route here
 
+@app.route("/artistquery/<artist_id>")
+def artistquery(artist_id):
+    """
+    Fetches the first 20 tracks from the Chinook database
+    and returns them as an HTML table.
+    Route: /viewdb
+    """
+    rows = execute_query("""
+        SELECT ArtistId, Artist.Name, Track.Name, UnitPrice
+        FROM Artist
+        WHERE ArtistId = artist_id
+        JOIN Album USING (ArtistID)
+        JOIN Track USING (AlbumID)
+        LIMIT 20
+    """)
+    return display_html(rows)
+
 # TODO: Section 3 — add your /pricequerytextbox GET and POST routes here
 
 # TODO: Section 3 — add your /timequerytextbox GET and POST routes here
